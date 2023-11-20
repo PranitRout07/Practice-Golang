c09d488880df2df402a8d14ccec5a11262094f68
PranitRout07 <pranitrout72@gmail.com>
2023-11-20 18:50:33 +0530

    add a fuzzy test

--- Testing/reverse_test.go
+++ Testing/reverse_test.go
@@ -2,24 +2,41 @@ package main
 
 import (
     "testing"
+    "unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-    testcases := []struct {
-        in, want string
-    }{
-        {"Hello, world", "dlrow ,olleH"},
-        {" ", " "},
-        {"!12345", "54321!"},
-		{"lag", "gal"},
-		{"f123!", "!321f"},
-		{"abcdcde$$$", "$$$edcdcba"},
-		{"123","321"},
-    }
+// func TestReverse(t *testing.T) {
+//     testcases := []struct {
+//         in, want string
+//     }{
+//         {"Hello, world", "dlrow ,olleH"},
+//         {" ", " "},
+//         {"!12345", "54321!"},
+// 		{"lag", "gal"},
+// 		{"f123!", "!321f"},
+// 		{"abcdcde$$$", "$$$edcdcba"},
+// 		{"123","321"},
+//     }
+//     for _, tc := range testcases {
+//         rev := Reverse(tc.in)
+//         if rev != tc.want {
+//                 t.Errorf("Reverse: %q, want %q", rev, tc.want)
+//         }
+//     }
+// }
+func FuzzReverse(f *testing.F) {
+    testcases := []string{"Hello, world", " ", "!12345"}
     for _, tc := range testcases {
-        rev := Reverse(tc.in)
-        if rev != tc.want {
-                t.Errorf("Reverse: %q, want %q", rev, tc.want)
-        }
+        f.Add(tc)  // Use f.Add to provide a seed corpus
     }
+    f.Fuzz(func(t *testing.T, orig string) {
+        rev := Reverse(orig)
+        doubleRev := Reverse(rev)
+        if orig != doubleRev {
+            t.Errorf("Before: %q, after: %q", orig, doubleRev)
+        }
+        if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+            t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+        }
+    })
 }
