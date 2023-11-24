a6f3c41f3640b3fbef469d71ec48d85ccb3bc9b9
PranitRout07 <pranitrout72@gmail.com>
2023-11-24 19:46:41 +0530

    return Json response

--- Web-app-using-Fiber/server.go
+++ Web-app-using-Fiber/server.go
@@ -5,13 +5,16 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 )
-
+type JSONTextResponse struct{
+	Message string
+}
 func main() {
 	fmt.Println("hello")
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello!!!")
+		// return c.SendString("Hello!!!")
+		return c.JSON(JSONTextResponse{Message: "Hello,whats up"})
 	})
 	app.Listen(":8080")
 
