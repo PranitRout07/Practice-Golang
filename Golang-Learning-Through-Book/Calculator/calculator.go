859f8c74bb6aedf85c8c04402937c2aa1ea6f031
PranitRout07 <pranitrout72@gmail.com>
2024-03-21 20:32:36 +0530

    added more functions and test cases

--- Golang-Learning-Through-Book/Calculator/calculator.go
+++ Golang-Learning-Through-Book/Calculator/calculator.go
@@ -6,3 +6,10 @@ func Add(a, b float64) float64 {
 
 // automatic format the go code :- gofmt -w .\calculator.go
 // test the code :- go test
+
+func Subtract(a, b float64) float64 {
+	return a-b
+}
+func Multiply(a, b float64) float64 {
+	return a*b
+}
--- Golang-Learning-Through-Book/Calculator/calculator_test.go
+++ Golang-Learning-Through-Book/Calculator/calculator_test.go
@@ -1,13 +1,68 @@
 package calculator_test
+
 import (
-"calculator"
-"testing"
+	"calculator"
+	"log"
+	"testing"
 )
+
 func TestAdd(t *testing.T) {
-t.Parallel()
-var want float64 = 4
-got := calculator.Add(2, 2)
-if want != got {
-t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	TestCases := []testCase{
+		{a: 10, b: 10, want: 20},
+		{a: 5, b: 0, want: 5},
+		{a: -5, b: 5, want: 0},
+	}
+	for _, i := range TestCases {
+
+		got := calculator.Add(i.a, i.b)
+		if i.want != got {
+			t.Errorf("Add(%f,%f) : want %f, got %f", i.a, i.b, i.want, got)
+		}
+		log.Println("Test Addition Successful")
+	}
 }
+
+func TestSubtract(t *testing.T) {
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	TestCases := []testCase{
+		{a: 10, b: 10, want: 0},
+		{a: 5, b: 0, want: 5},
+		{a: -5, b: 5, want: -10},
+	}
+	for _, i := range TestCases {
+
+		got := calculator.Subtract(i.a, i.b)
+		if i.want != got {
+			t.Errorf("Subtract(%f,%f) : want %f, got %f", i.a, i.b, i.want, got)
+		}
+		log.Println("Test Subtraction Successful")
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	TestCases := []testCase{
+		{a: 10, b: 10, want: 100},
+		{a: 5, b: 0, want: 0},
+		{a: -5, b: 5, want: -25},
+	}
+	for _, i := range TestCases {
+
+		got := calculator.Multiply(i.a, i.b)
+
+		if i.want != got {
+			t.Errorf("Multiply(%f,%f) : want %f, got %f", i.a, i.b, i.want, got)
+		}
+		log.Println("Test Multiplication Successful")
+	}
 }
