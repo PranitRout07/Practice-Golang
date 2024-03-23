8a52a6335ae1c347f48d93922b770e5d154b4ea9
PranitRout07 <pranitrout72@gmail.com>
2024-03-23 20:48:35 +0530

    more precise test functions

--- Golang-Learning-Through-Book/Calculator/calculator_test.go
+++ Golang-Learning-Through-Book/Calculator/calculator_test.go
@@ -4,7 +4,12 @@ import (
 	"calculator"
 	"log"
 	"testing"
+	"math"
 )
+
+func closeEnough(a, b, gap float64) bool {
+	return math.Abs(a-b) <= gap
+}
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
@@ -70,7 +75,6 @@ func TestMultiply(t *testing.T) {
 	}
 }
 
-
 func TestDivide(t *testing.T) {
 	t.Parallel()
 	type testCase struct {
@@ -81,16 +85,18 @@ func TestDivide(t *testing.T) {
 		{a: 10, b: 10, want: 1},
 		{a: 5, b: 1, want: 5},
 		{a: -5, b: 5, want: -1},
+		{a: 1 , b: 3 , want: 0.333333},
 		// {a: 1 , b : 0 , want : 0},
 	}
 	for _, i := range TestCases {
 
 		got, err := calculator.Divide(i.a, i.b)
 
-		if err!=nil{
+		if err != nil {
 			t.Fatalf("want no error for valid input, got %v", err)
 		}
-		if i.want != got {
+
+		if i.want != got && !closeEnough(i.want,got,0.001) {
 			t.Errorf("Divide(%f,%f) : want %f, got %f", i.a, i.b, i.want, got)
 		}
 		log.Println("Test Divide Successful")
