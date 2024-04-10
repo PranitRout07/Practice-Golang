336f6aee9ccec490b4534329e31a307eaa42829b
PranitRout07 <pranitrout72@gmail.com>
2024-04-10 20:01:21 +0530

    list the containers and its logs

--- Docker-container-logs/main.go
+++ Docker-container-logs/main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+
 	"context"
 	"fmt"
 
-	"github.com/docker/docker/api/types/container"
+	"io"
+	"os"
+	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
 func main() {
-	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
-	defer apiClient.Close()
-
-	containers, err := apiClient.ContainerList(context.Background(), container.ListOptions{All: true})
+	defer cli.Close()
+	//Listing all the containers and their logs
+	containers, err := cli.ContainerList(ctx, containertypes.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
+	for _, container := range containers {
+		fmt.Println("CONTAINER NAME: ", container.Names)
+		fmt.Println("------------------------------")
+		options := containertypes.LogsOptions{ShowStdout: true,Timestamps: true , Details: true}
+		out, err := cli.ContainerLogs(ctx, container.ID, options)
+		if err != nil {
+			panic(err)
+		}
+		defer out.Close()
 
-	for _, ctr := range containers {
-		fmt.Printf("%s %s (status: %s)\n", ctr.ID, ctr.Image, ctr.Status)
+		// Copy container logs directly to standard output
+		if _, err := io.Copy(os.Stdout, out); err != nil {
+			panic(err)
+		}
+
 	}
+
 }
