8c270e2d27da35e1687c448573a930c8df3e4820
PranitRout07 <pranitrout72@gmail.com>
2024-04-17 15:23:30 +0530

    crud-using-gin-package

---
+++ CRUD-using-gin-package/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+type api struct{
+	Name string `json:"name"`
+	Email string `json:"email"`
+}
+var data api 
+
+
+func main() {
+  r := gin.Default()
+  r.GET("/get", getData)
+  r.POST("/post", postData)
+  r.PUT("/put", putData)
+  r.DELETE("/delete", deleteData)
+  r.Run(":9090") 
+}
+
+func getData(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": data,
+    })
+}
+
+func postData(c *gin.Context) {
+	err := c.BindJSON(&data)
+	if err!=nil{
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Something is wrong",
+		  })
+
+	}
+    c.JSON(http.StatusOK, gin.H{
+      "message": data,
+    })
+}
+
+func putData(c *gin.Context) {
+	err := c.BindJSON(&data)
+	if err!=nil{
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Something went wrong",
+		  })
+
+	}
+    c.JSON(http.StatusOK, gin.H{
+      "message": data,
+    })
+}
+
+func deleteData(c *gin.Context) {
+	data = api{}
+    c.JSON(http.StatusOK, gin.H{
+      "message": data,
+    })
+}
