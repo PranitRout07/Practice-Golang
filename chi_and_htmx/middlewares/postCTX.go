fe3d4d5a9128d0e8bb47fb547d9192270af817bc
PranitRout07 <pranitrout72@gmail.com>
2024-06-16 13:33:52 +0530

    updated postctx middleware

--- chi_and_htmx/middlewares/postCTX.go
+++ chi_and_htmx/middlewares/postCTX.go
@@ -14,13 +14,12 @@ import (
 type contextKey string
 
 const PostsKey contextKey = "posts"
-var ID string
+
 func PostCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var posts models.Posts
 		id := chi.URLParam(r, "id")
 		log.Println("URL parameter 'id':", id)
-		ID = id
 		if id := chi.URLParam(r, "id"); id != "" {
 			stmt := "select * from posts where id=$1"
 			log.Println(id, "hhhhhhh")
